244a0ff55037766e13ce8929e434f329c6522ccf
Gennadiy <genakisil@gmail.com>
2019-11-06 14:34:25 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using HidSharp;
+
+namespace HID
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Older.Prop1 p = new Older.Prop1();
+
+            Console.WriteLine(p.Opc1());
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+
+    
+}
+
+namespace Older
+{
+    class Prop1 : Prop
+    {
+        public override string Opc1()
+        {
+            return "This method in base class overriden.";
+        }
+    }
+    abstract class Prop
+    {
+        public static string a = "1";
+
+        public string Desc
+        {
+            get
+            {
+                return a;
+            }
+        }
+
+        public string Opc()
+        {
+            return a;
+        }
+        public abstract string Opc1();
+    }
+}
